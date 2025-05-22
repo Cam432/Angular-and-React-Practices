3f5dcc493eb0e1dfd18e2a6d73ee036d91e66274
Cam432 <villarrealcami9@gmail.com>
2025-05-22 16:55:46 -0300

    prueba5

---
+++ Typescrit-introduction/src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = 'Holiwis'; //Mensaje en pagina
+
+console.log('Que andamos intentando hacer lokito?') //Mensaje en consola
